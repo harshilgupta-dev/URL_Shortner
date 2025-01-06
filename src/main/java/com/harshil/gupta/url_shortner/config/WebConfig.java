e738cb20cd99260d62e6d22967a5d7c2f086f700
Harshil Gupta <harshil.gupta@geminisolutions.com>
2025-01-06 22:58:45 +0530

    update cors

--- src/main/java/com/harshil/gupta/url_shortner/config/WebConfig.java
+++ src/main/java/com/harshil/gupta/url_shortner/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow your frontend origin
+                .allowedOrigins("http://localhost:3000","http://18.208.214.158:3000") // Allow your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
